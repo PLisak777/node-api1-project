39f5e10bf05264099264b5a8efcac0df7341bbf8
Phil <philip-lisak@lambdastudents.com>
2020-10-28 18:47:19 -0700

    MVP Complete

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ server.post('/api/users', (req, res) => {
 	}
 });
 
+// read
 server.get('/api/users', (req, res) => {
 	try {
 		res.status(200).json(users)
@@ -47,6 +48,65 @@ server.get('/api/users', (req, res) => {
 	}
 })
 
+// read
+server.get('/api/users/:id', (req, res) => {
+	try {
+		const { id } = req.params;
+		const found = users.find(user => user.id === id)
 
+		if (found) {
+			res.status(200).json(found)
+		}
+		else {
+			res.status(404).json({ message: "The user with the specified ID does not exist." })
+		}
+	}
+	catch (err) {
+		res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+	}
+})
+
+// delete
+server.delete('api/users/:id', (req, res) => {
+	try {
+		const { id } = req.params
+		const found = users.find(user => user.id === id)
+
+		if (found) {
+			users = users.filter(user => user.id !== id)
+			res.status(204).json(found)
+		}
+		else {
+			res.status(404).json({ message: "The user with the specified ID does not exist." })
+		}
+	}
+	catch (err) {
+		res.status(500).json({ errorMessage: "The user could not be removed" })
+	}
+})
+
+// update
+server.put('/api/users/:id', (req, res) => {
+	try {
+		const { id } = req.params
+		const found = users.find(user => user.id === id)
+		const userChanges = req.body
+
+		if (found) {
+			if (userChanges.name && userChanges.bio) {
+				userChanges.id = id
+				users = users.map(user => user.id === id ? userChanges : user)
+				res.status(200).json(userChanges)
+			} else {
+				res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+			}
+		} else {
+			res.status(404).json({ message: "The user with the specified ID does not exist." })
+		}
+	}
+	catch (err) {
+		res.status(500).json({ errorMessage: "The user information could not be modified." })
+	}
+})
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
