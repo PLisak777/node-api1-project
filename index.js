244758399b403b9e1da39467df8bfee6d82f923b
Phil <philip-lisak@lambdastudents.com>
2020-10-28 18:31:25 -0700

    First .post and .get complete

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const shortid = require('shortid');
-
-let users = [{
-	id: shortid.generate(),
-	name: 'Dopey',
-	bio: '7th Dwarf, if that dwarf weilds an axe'
-}];
-
 const server = express();
+
+let users = [];
+
+const PORT = 5000;
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+	res.status(200).send('Hello World');
+});
 
 // C - create (POST)
 // R - read (GET)
@@ -19,11 +19,34 @@ server.use(express.json());
 
 // create
 server.post('/api/users', (req, res) => {
-	const { name, bio } = req.params;
-
-	const newUser = req.body;
-	users.push(newUser);
-
-	res.status(201).json(newUser);
+	let newUser = req.body;
+	if (newUser.name && newUser.bio) {
+		try {
+			newUser.id = shortid.generate();
+			users.push(newUser);
+			res.status(201).json(newUser);
+		} catch (err) {
+			res.status(500).json({
+				errorMessage:
+					'There was an error while saving the user to the database',
+			});
+		}
+	} else {
+		res
+			.status(400)
+			.json({ errorMessage: 'Please provide name and bio for the user.' });
+	}
 });
 
+server.get('/api/users', (req, res) => {
+	try {
+		res.status(200).json(users)
+	}
+	catch (err) {
+		res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+	}
+})
+
+
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
